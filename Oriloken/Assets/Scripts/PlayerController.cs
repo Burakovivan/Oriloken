0abaa85ba2875b4f64dba68b7fb90d92acaade0e
MicrosoftSQLServer2014 <sanyadav.sd@gmail.com>
2016-04-29 18:23:55 +0300

    Player script and ball physics are added

---
+++ Oriloken/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float playerVelocity;
+    private Vector3 playerPosition;
+    public float boundary;
+
+    // используйте этот метод для инициализации
+    void Start()
+    {
+        // получим начальную позицию платформы
+        playerPosition = gameObject.transform.position;
+    }
+
+    // Update вызывается при отрисовке каждого кадра игры
+    void Update()
+    {
+        // горизонтальное движение
+        playerPosition.x += Input.GetAxis("Horizontal") * playerVelocity;
+
+        // выход из игры
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // обновим позицию платформы
+        transform.position = playerPosition;
+
+        // проверка выхода за границы
+        if (playerPosition.x < -boundary)
+        {
+            transform.position = new Vector3(-boundary, playerPosition.y, playerPosition.z);
+        }
+        if (playerPosition.x > boundary)
+        {
+            transform.position = new Vector3(boundary, playerPosition.y, playerPosition.z);
+        }
+    }
+}
